e9c6ecddf83369c850d210e619001ea47840729c
Simon Shephard <simonshephard@hotmail.com>
2018-10-19 15:43:10 +0100

    adds quicksort algo to sort shop stock by price

--- src/main/java/musicShop/MusicShop.java
+++ src/main/java/musicShop/MusicShop.java
@@ -18,6 +18,10 @@ public class MusicShop {
         return name;
     }
 
+    public ArrayList<ISell> getStock() {
+        return stock;
+    }
+
     public int stockCount() {
         return this.stock.size();
     }
@@ -61,4 +65,44 @@ public class MusicShop {
         }
         return totalMarkupMoney;
     }
+
+    public void sortStockByPrice() {
+        sort(this.stock, 0, this.stock.size()-1);
+    }
+
+
+    // sort method recursively partitions and sorts by price
+    private void sort(ArrayList<ISell> stock, int lowIndex, int highIndex) {
+        if (lowIndex < highIndex) {
+            int partitionIndex = partition(stock, lowIndex, highIndex);
+            sort(stock, lowIndex, partitionIndex-1);
+            sort(stock, partitionIndex+1, highIndex);
+        }
+    }
+
+    // partition method partitions ArrayList around pivot (last) item
+    // and returns resulting index of pivot
+    private int partition(ArrayList<ISell> stock, int lowIndex, int highIndex) {
+
+        double pivotPrice = stock.get(highIndex).getPrice();
+        int i = (lowIndex-1);
+        for (int j = lowIndex; j < highIndex; j++) {
+            if (stock.get(j).getPrice() <= pivotPrice) {
+                i++;
+                ISell temp = stock.get(i);
+                stock.set(i, stock.get(j));
+                stock.set(j, temp);
+            }
+        }
+
+        ISell temp = stock.get(i+1);
+        stock.set(i+1, stock.get(highIndex));
+        stock.set(highIndex, temp);
+
+        return i+1;
+    }
+
+
+
+
 }
--- src/test/java/MusicShopTest.java
+++ src/test/java/MusicShopTest.java
@@ -1,4 +1,5 @@
 import musicShop.MusicShop;
+import musicShop.QuickSort;
 import musicShop.accessories.GuitarStrings;
 import musicShop.instruments.Guitar;
 import musicShop.instruments.InstrumentType;
@@ -64,5 +65,54 @@ public class MusicShopTest {
         assertEquals(44.00, musicShop.getTotalMarkupMoneyUsingItemMethod(), 0.01);
     }
 
+    @Test
+    public void sortStock() {
+
+        Guitar guitar1 = new Guitar(InstrumentType.PERCUSSION, "Wood", "Maple", "StrumStrum", 51.55, 65.55, 6);
+        Guitar guitar2 = new Guitar(InstrumentType.PERCUSSION, "Wood", "Maple", "StrumStrum", 51.55, 55.55, 6);
+        Guitar guitar3 = new Guitar(InstrumentType.PERCUSSION, "Wood", "Maple", "StrumStrum", 51.55, 45.55, 6);
+        Guitar guitar4 = new Guitar(InstrumentType.PERCUSSION, "Wood", "Maple", "StrumStrum", 51.55, 35.55, 6);
+        Guitar guitar5 = new Guitar(InstrumentType.PERCUSSION, "Wood", "Maple", "StrumStrum", 51.55, 25.55, 6);
+        GuitarStrings guitarStrings1 = new GuitarStrings("StandardElectric", "Steel", "Silver", 1.99, 59.99, 6);
+        GuitarStrings guitarStrings2 = new GuitarStrings("StandardElectric", "Steel", "Silver", 1.99, 49.99, 6);
+        GuitarStrings guitarStrings3 = new GuitarStrings("StandardElectric", "Steel", "Silver", 1.99, 39.99, 6);
+        GuitarStrings guitarStrings4 = new GuitarStrings("StandardElectric", "Steel", "Silver", 1.99, 29.99, 6);
+        GuitarStrings guitarStrings5 = new GuitarStrings("StandardElectric", "Steel", "Silver", 1.99, 19.99, 6);
+        musicShop.addStock(guitar1);
+        musicShop.addStock(guitar2);
+        musicShop.addStock(guitar3);
+        musicShop.addStock(guitar4);
+        musicShop.addStock(guitar5);
+        musicShop.addStock(guitarStrings1);
+        musicShop.addStock(guitarStrings2);
+        musicShop.addStock(guitarStrings3);
+        musicShop.addStock(guitarStrings4);
+        musicShop.addStock(guitarStrings5);
+
+        assertEquals(guitar1, musicShop.getStock().get(0));
+        assertEquals(guitar2, musicShop.getStock().get(1));
+        assertEquals(guitar3, musicShop.getStock().get(2));
+        assertEquals(guitar4, musicShop.getStock().get(3));
+        assertEquals(guitar5, musicShop.getStock().get(4));
+        assertEquals(guitarStrings1, musicShop.getStock().get(5));
+        assertEquals(guitarStrings2, musicShop.getStock().get(6));
+        assertEquals(guitarStrings3, musicShop.getStock().get(7));
+        assertEquals(guitarStrings4, musicShop.getStock().get(8));
+        assertEquals(guitarStrings5, musicShop.getStock().get(9));
+
+        musicShop.sortStockByPrice();
+
+        assertEquals(guitarStrings5, musicShop.getStock().get(0));
+        assertEquals(guitar5, musicShop.getStock().get(1));
+        assertEquals(guitarStrings4, musicShop.getStock().get(2));
+        assertEquals(guitar4, musicShop.getStock().get(3));
+        assertEquals(guitarStrings3, musicShop.getStock().get(4));
+        assertEquals(guitar3, musicShop.getStock().get(5));
+        assertEquals(guitarStrings2, musicShop.getStock().get(6));
+        assertEquals(guitar2, musicShop.getStock().get(7));
+        assertEquals(guitarStrings1, musicShop.getStock().get(8));
+        assertEquals(guitar1, musicShop.getStock().get(9));
+
+    }
 
 }
